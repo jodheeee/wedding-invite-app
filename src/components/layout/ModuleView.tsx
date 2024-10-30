29a279efdd26354d024319645543e8cc21583169
Dae-Hee <soso99700@naver.com>
2024-10-30 12:12:59 +0900

    feat: add ModuleView layout component

--- src/components/layout/ModuleView.tsx
+++ src/components/layout/ModuleView.tsx
@@ -6,8 +6,8 @@ interface ModuleViewProps {
 
 const ModuleView: FC<ModuleViewProps> = ({ children }) => (
   <div className="flex flex-col flex-1 bg-slate-50 h-screen">
-    <div className="relative bg-white h-screen md:h-600 w-full md:w-360 md:m-auto overflow-y-auto">
-      <div className="px-30">{children}</div>
+    <div className="relative bg-white h-screen md:h-650 w-full md:w-360 md:m-auto">
+      {children}
     </div>
   </div>
 );
