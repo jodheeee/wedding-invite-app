326243dae6db7b9a387c51917dae07b828ccaa3a
Dae-Hee <soso99700@naver.com>
2024-11-04 11:09:19 +0900

    feat: add global style screen-dvh

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,14 +6,19 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     screens: {
-      md: pxToRem(600),
+      md: pxToRem(500),
     },
     spacing: {
       ...basicSpace,
       360: pxToRem(360),
       650: pxToRem(650),
+      800: pxToRem(800),
     },
-    extend: {},
+    extend: {
+      height: {
+        'screen-dvh': '100dvh',
+      },
+    },
   },
   plugins: [],
 };
