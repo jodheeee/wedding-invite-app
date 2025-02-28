0f19fee7735d314a0fc88d570779f33cf310ea2f
Dae-Hee <soso99700@naver.com>
2025-03-01 00:29:12 +0900

    feat: Implement background music component

---
+++ src/components/common/BackgroundMusic.tsx
@@ -0,0 +1,36 @@
+import { useState, useRef } from 'react';
+import backgroundMusic from '@/assets/background-music.mp3';
+
+const BackgroundMusic = () => {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const audioRef = useRef<HTMLAudioElement | null>(null);
+
+  const togglePlay = () => {
+    if (audioRef.current) {
+      if (isPlaying) {
+        audioRef.current.pause();
+      } else {
+        audioRef.current.play();
+      }
+      setIsPlaying(!isPlaying);
+    }
+  };
+
+  return (
+    <div className="px-15 pb-4 pt-3 w-fit border border-black rounded-full cursor-pointer ml-auto">
+      <button
+        onClick={togglePlay}
+        className="text-sm"
+      >
+        {isPlaying ? '⏸︎ Stop' : '▶︎ Play'}
+      </button>
+      <audio
+        ref={audioRef}
+        src={backgroundMusic}
+        loop
+      />
+    </div>
+  );
+};
+
+export default BackgroundMusic;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 const pxToRem = (px) => px / 16 + 'rem';
-const basicSpace = Array.from(Array(101)).map((_, index) => pxToRem(index));
+const basicSpace = Array.from(Array(200)).map((_, index) => pxToRem(index));
 
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
@@ -10,18 +10,31 @@ export default {
     },
     spacing: {
       ...basicSpace,
-      110: pxToRem(110),
-      120: pxToRem(120),
-      130: pxToRem(130),
-      160: pxToRem(160),
+      280: pxToRem(280),
       360: pxToRem(360),
       650: pxToRem(650),
+      700: pxToRem(700),
+      760: pxToRem(760),
       800: pxToRem(800),
+      900: pxToRem(900),
     },
-    extend: {
-      height: {
-        'screen-dvh': '100dvh',
-      },
+    borderWidth: {
+      DEFAULT: '1px',
+      0: '0',
+      1: pxToRem(1),
+      2: pxToRem(2),
+      3: pxToRem(3),
+      4: pxToRem(4),
+      5: pxToRem(5),
+      6: pxToRem(6),
+      7: pxToRem(7),
+      8: pxToRem(8),
+      9: pxToRem(9),
+      10: pxToRem(10),
+    },
+    extend: {},
+    fontFamily: {
+      MapoDacapo: ['MapoDacapo'],
     },
   },
   plugins: [],
