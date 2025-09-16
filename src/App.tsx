17a708e4efee526edff319c0b1f0cc05cf36716a
Dae-Hee <75158767+jodheeee@users.noreply.github.com>
2025-09-16 06:17:39 +0000

    feat: add SpeedInsights

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import Main from './view/Main';
 import Gallery from './view/Gallery';
 import Map from './view/Map';
 import Message from './view/Message';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 const App = () => {
   return (
     <ModuleView>
+      <SpeedInsights />
       <Main />
       <Gallery />
       <Map />
